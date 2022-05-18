c3222476099fc0be29744e853f8bcb8ef531ad03
SmeetsSimon <simonsmeets99@gmail.com>
2022-05-18 15:04:52 +0200

    email port

--- Weerstation/settings.py
+++ Weerstation/settings.py
@@ -148,7 +148,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #myaccount.google.com/apppasswords
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
+EMAIL_PORT = 587
 EMAIL_HOST_USER = 'simonsmeets123@gmail.com'
 EMAIL_HOST_PASSWORD = 'PASSWORD' 
 EMAIL_USE_TLS = False
