ccdac6515bd559450e6d736b5984300b2e26e7f3
SmeetsSimon <simonsmeets99@gmail.com>
2022-06-03 12:16:48 +0200

    commit

--- Weerstation/settings.py
+++ Weerstation/settings.py
@@ -150,7 +150,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'simonsmeets123@gmail.com'
-EMAIL_HOST_PASSWORD = '' #'PASSWORD' 
+EMAIL_HOST_PASSWORD = 'Testgip123' #'PASSWORD' 
 EMAIL_USE_TLS = True
 
 
